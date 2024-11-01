5038dbcacc346f7e82094bb4b1255c06c3461b95
Chase_Haynes <chase.haynez@gmail.com>
2024-11-01 06:56:32 -0500

    new

---
+++ src/components/Phone.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+// we set it up like this so <Phone /> can be re used with any props that are needed such as hover or various things
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        className="pointer-events-none z-50 select-none"
+        alt="phone image"
+      />
+    </div>
+  );
+};
+
+export default Phone;
